23e6c280240c102f44190d0bbf17ca1bb324c6a3
BryanCachimuel <bryanloyo56@gmail.com>
2022-09-25 22:14:23 -0500

    agregando la parte del html para que funcione la parte de php de registro de usuarios

--- registrar.php
+++ registrar.php
@@ -103,4 +103,104 @@ if(isset($_POST['submit']))
 }
 ?>
  
- 
+ <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Sitio Web de Libros</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+     
+
+        <div id="layoutSidenav_content">
+               
+            <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header bg-dark">
+                                        <h3 class="text-center text-white font-weight-light my-4">Registrar Usuarios</h3>
+                                    </div>
+                                    <div class="card-body">
+                                    <?php 
+                                        if(isset($errors) && count($errors) > 0)
+                                        {
+                                            foreach($errors as $error_msg)
+                                            {
+                                                echo '<div class="alert alert-danger">'.$error_msg.'</div>';
+                                            }
+                                        }
+                                        
+                                        if(isset($success))
+                                        {
+                                            
+                                            echo '<div class="alert alert-success">'.$success.'</div>';
+                                        }
+                                    ?>
+                                        <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" name="nombre" id="inputFirstName" type="text" placeholder="Enter first name" />
+                                                        <label class="small mb-1" for="inputFirstName">Nombre</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" name="apellido" id="inputLastName" type="text" placeholder="Enter last name" />
+                                                        <label class="small mb-1" for="inputLastName">Apellido</label>
+                                                    </div>
+                                                </div>
+                                             </div>
+                                             
+                                             <div class="form-floating mb-3">
+                                                <input class="form-control" name="email" id="inputEmailAddress" type="email" aria-describedby="emailHelp" placeholder="Enter email address" />
+                                                <label class="small mb-1" for="inputEmailAddress">Email</label>
+                                            </div>
+                                            
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Enter password" />
+                                                        <label class="small mb-1" for="inputPassword">Password</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" name="repassword" id="inputConfirmPassword" type="password" placeholder="Confirm password" />
+                                                         <label class="small mb-1" for="inputConfirmPassword">Confirme el  Password</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4 mb-0">
+                                                <button type="submit" name="submit" class="btn btn-primary">Registrarse</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <!--<div class="card-footer text-center">
+                                        <div class="small"><a href="login.php">¿Tener una cuenta? Ir a iniciar sesión</a></div>
+                                    </div>-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </main>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
